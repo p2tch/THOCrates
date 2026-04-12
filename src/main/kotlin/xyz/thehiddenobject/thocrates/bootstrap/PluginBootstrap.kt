4f2f06babfc9c8f4d2061e4f7fac739f495d41cc
p2tch <231753395+p2tch@users.noreply.github.com>
2026-04-12 14:44:57 +0200

    feat: initial Paper plugin setup

---
+++ src/main/kotlin/xyz/thehiddenobject/thocrates/bootstrap/PluginBootstrap.kt
@@ -0,0 +1,14 @@
+package xyz.thehiddenobject.thocrates.bootstrap
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PluginBootstrap : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
