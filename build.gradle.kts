f32df59636aa2260ce245b3f2a3f18fb67da59cb
p2tch <231753395+p2tch@users.noreply.github.com>
2026-04-12 15:06:14 +0200

    chore: add guice to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 dependencies {
     compileOnly(libs.paper.api)
     implementation(libs.kotlin.stdlib)
+    implementation(libs.guice)
 }
 
 kotlin {
--- src/main/kotlin/xyz/thehiddenobject/thocrates/bootstrap/PluginBootstrap.kt
+++ src/main/kotlin/xyz/thehiddenobject/thocrates/bootstrap/PluginBootstrap.kt
@@ -3,12 +3,11 @@ package xyz.thehiddenobject.thocrates.bootstrap
 import org.bukkit.plugin.java.JavaPlugin
 
 class PluginBootstrap : JavaPlugin() {
-
     override fun onEnable() {
-        // Plugin startup logic
+
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+
     }
 }
